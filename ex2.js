492e3c0e360c9df31320728faf55d829b9cbd242
shinmike <shinmike90@gmail.com>
2017-06-05 21:26:58 +0000

    added alternative solution, that does not need variable 'stream'

--- ex2.js
+++ ex2.js
@@ -3,20 +3,25 @@ var fs = require('fs');
 
 console.log("Downloading image...");
 
-var stream = request.get('https://sytantris.github.io/http-examples/future.jpg')
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+   .on('error', function (err) {
+      throw err;
+   })
+   .on('response', function (response) {
+      console.log('Response Status Code: ', response.statusCode);
+      console.log('Response Message: ', response.statusMessage);
+      console.log('Response Content Type: ', response.headers['content-type']);
+   })
+   .pipe(fs.createWriteStream('./future.jpg'))
+   .on('finish', function(){
+      console.log("Downloading complete.");
+   });
 
-.on('error', function (err) {
-   throw err;
-})
 
-.on('response', function (response) {
-   console.log('Response Status Code: ', response.statusCode);
-   console.log('Response Message: ', response.statusMessage);
-   console.log('Response Content Type: ', response.headers['content-type']);
-})
+// *** could also be written using variable. ***
 
-.pipe(fs.createWriteStream('./future.jpg'));
+// var stream = request.get('https://sytantris.github.io/http-examples/future.jpg')
 
-stream.on('finish', function(){
-   console.log("Downloading complete.");
-});
+// stream.on('finish', function(){
+//    console.log("Downloading complete.");
+// });
